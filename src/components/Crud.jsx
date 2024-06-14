38d72ff3ecee6e65df720223948ac40c152caba8
madhuraj <madhurajmadu693@gmail.com>
2024-06-14 12:56:35 +0530

    layout design is modified

--- src/components/Crud.jsx
+++ src/components/Crud.jsx
@@ -13,16 +13,15 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function Crud() {
 
   const [animeData, setAnimeData] = useState([]);
-
   const [show, setShow] = useState(false);
 
   // this is for the adding the New data 
   const [animieName, setanimeName] = useState('');
   const [animieDesc, setanimeDesc] = useState('');
-  const [animieRating, setanimeRating] = useState();
+  const [animieRating, setanimeRating] = useState(0);
 
   //this is for editig the existing data
-  const [editId, setEditId] = useState();
+  const [editId, setEditId] = useState(0);
 
   const [editAnimieName, setEditAnimieName] = useState('');
   const [editAnimieDesc, setEditAnimieDesc] = useState('');
@@ -32,7 +31,7 @@ export default function Crud() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   //for getting data from the get operation
-  const getData=()=>{
+  const getData = () => {
     axios.get('/api/Animie')
       .then((response) => {
         setAnimeData(response.data);
@@ -52,63 +51,63 @@ export default function Crud() {
       <td>{anime.name}</td>
       <td>{anime.description}</td>
       <td>{anime.rating}</td>
-      <td>
-        <button onClick={() => handleEditButton(anime.id)} className='bg-primary'>Edit</button>
-        {/* <button onClick={() => handleAddButton(anime.id)} className='bg-success'>Add</button> */}
-        <button onClick={() => handleDelete(anime.id)} className='bg-danger'>Delete</button>
-      </td>
+      <div className=' mx-2 d-flex bg-info'>
+        <button onClick={() => handleEditButton(anime.id)} className=' btn btn-primary border px-4 mx-2'>Edit</button>
+        <button onClick={() => handleDelete(anime.id)} className='btn btn-danger border px-3'>Delete</button>
+      </div>
     </tr>
+
   ));
 
   const handleEditButton = (id) => {
-    handleShow();
+
     axios.get(`https://localhost:7193/api/Animie/${id}`)
-    .then((result)=>{
-      setEditAnimieName(result.data.animieName)
-      // console.log(result.data.animieName)
-      
-      setEditAnimieDesc(result.data.animieDesc)
-      setEditAnimieRating(result.data.animieRating)
-      setEditId(id)
-    })
-    .catch((error)=>{
-      console.log(error)
-    })
+      .then((result) => {
+        setEditAnimieName(result.data.name)
+        console.log(result)
+
+        setEditAnimieDesc(result.data.description)
+        setEditAnimieRating(result.data.rating)
+        setEditId(id)
+
+        handleShow();
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
- 
   const handleAddButton = (id) => {
     handleShow();
-    
+
   }
-  const handleDelete=(id)=>{
+  const handleDelete = (id) => {
     axios.delete(`https://localhost:7193/api/Animie?id=${id}`)
-    .then((result)=>{
-      if(result.status==200)
-      {
-        toast.success('employee has deleted successfully');
-        getData();
-      }     
-    }).catch((error)=>
-    {
-      toast.error(error);
-    })
+      .then((result) => {
+        if (result.status == 200) {
+          toast.success('employee has deleted successfully');
+          getData();
+        }
+      }).catch((error) => {
+        toast.error(error);
+      })
   }
-  
+
   const handleUpdate = () => {
-    const url=`https://localhost:7193/api/Animie/${editId}`;
-    const data= {
-      "id":editId,
+    const url = 'https://localhost:7193/api/Animie';
+    const data = {
+      "id": editId,
       "name": editAnimieName,
       "description": editAnimieDesc,
       "rating": editAnimieRating
     }
-    axios.put(url,data)
-    .then((result)=>{
-      setEditAnimieName(result.data.editAnimieName)  
-      setEditAnimieDesc(result.data.editAnimieDesc)
-      setEditAnimieRating(result.data.editAnimieRating)
-    })
+    axios.put(url, data)
+      .then((result) => {
+        setEditId(result.data.id)
+        setEditAnimieName(result.data.editAnimieName)
+        setEditAnimieDesc(result.data.editAnimieDesc)
+        setEditAnimieRating(result.data.editAnimieRating)
+      })
     getData()
     handleClose()
 
@@ -129,64 +128,69 @@ export default function Crud() {
         Clear();
         toast.success('New Animie has been added successfully');
       })
-
   }
-
   //to clear the data
   const Clear = () => {
     setanimeName('');
     setanimeDesc('');
     setanimeRating('');
   }
-
   return (
     <>
-      <Fragment>
-        <ToastContainer />
-        <Container>
-          <Row>
-            <Col><input type="text" className='form-control' placeholder='Enter the Animie' value={animieName} onChange={(e) => setanimeName(e.target.value)} /></Col>
-            <Col><input type="text" className='form-control' placeholder='Enter the Description' value={animieDesc} onChange={(e) => setanimeDesc(e.target.value)} /></Col>
-            <Col><input type="number" className='form-control' placeholder='Enter the Rating ' value={animieRating} onChange={(e) => setanimeRating(e.target.value)} /></Col>
-          </Row>
-          <button className='btn btn-primary' onClick={handleSave}>Add</button>
-          
-        </Container>
-        <Table striped>
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Animie</th>
-              <th>Description</th>
-              <th>Rating</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableRows}
-          </tbody>
-
-        </Table>
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Animie Update/Add </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
+      <div className='d-flex flex-column  bg-info '>
+        <h2 className='text-center'>Animax</h2>
+        <Fragment>
+          <ToastContainer />
+          <Container className='mt-5'>
             <Row>
-              <Col><input type="text" className='form-control' placeholder='Name' value={editAnimieName} onChange={(e) => setEditAnimieName(e.target.value)} /></Col>
-              <Col><input type="text" className='form-control' placeholder='Description' value={editAnimieDesc} onChange={(e) => setEditAnimieDesc(e.target.value)} /></Col>
-              <Col><input type="number" className='form-control' placeholder='Rating' value={editAnimieRating} onChange={(e) => setEditAnimieRating(e.target.value)} /></Col>
+              <Col><input type="text" className='form-control' placeholder='Enter the Animie' value={animieName} onChange={(e) => setanimeName(e.target.value)} /></Col>
+              <Col><input type="text" className='form-control' placeholder='Enter the Description' value={animieDesc} onChange={(e) => setanimeDesc(e.target.value)} /></Col>
+              <Col><input type="number" className='form-control' placeholder='Enter the Rating ' value={animieRating} onChange={(e) => setanimeRating(e.target.value)} /></Col>
             </Row>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleUpdate}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </Fragment>
+            <button className='btn btn-primary mt-2' onClick={handleSave}>Add</button>
+
+
+            <Table striped className='mt-5'>
+              <thead >
+                <tr style={{ width: '120px' }}>
+                  <th>Id</th>
+                  <th>Animie</th>
+                  <th>Description</th>
+                  <th>Rating</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableRows}
+              </tbody>
+
+            </Table>
+          </Container>
+
+
+          {/* model popup */}
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Animie Update/Add </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Row>
+                <Col><input type="text" className='form-control' placeholder='Name' value={editAnimieName} onChange={(e) => setEditAnimieName(e.target.value)} /></Col>
+                <Col><input type="text" className='form-control' placeholder='Description' value={editAnimieDesc} onChange={(e) => setEditAnimieDesc(e.target.value)} /></Col>
+                <Col><input type="number" className='form-control' placeholder='Rating' value={editAnimieRating} onChange={(e) => setEditAnimieRating(e.target.value)} /></Col>
+              </Row>
+
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleUpdate}>
+                Save Changes
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Fragment>
+      </div>
     </>
   )
 }
